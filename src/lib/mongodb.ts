2c2d8f65f679ac634119a4351ca9b01f1e9d6a92
jayesh <mahakalijayesh97@gmail.com>
2026-04-16 18:51:31 +0530

    I have now applied the final fix to the MongoDB driver.

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,7 +6,7 @@ const options = {};
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.DATABASE_URL && process.env.NEXT_PHASE !== 'phase-production-build') {
   throw new Error("Please add your Mongo URI to .env");
 }
 
@@ -22,7 +22,12 @@ if (process.env.NODE_ENV === "development") {
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  // Do not connect during build phase
+  if (process.env.NEXT_PHASE === 'phase-production-build') {
+     clientPromise = Promise.resolve(client);
+  } else {
+     clientPromise = client.connect();
+  }
 }
 
 export default clientPromise;
