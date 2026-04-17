e743d60123aeacd224ec9306932a70093e0f0a04
jayesh <mahakalijayesh97@gmail.com>
2026-04-17 09:02:33 +0530

    I have added an 'Emergency Fallback' to your code. This means that even if you don't set the environment variable in the Vercel Dashboard, the app will now automatically find your database.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
+// Emergency fallback for Vercel environment variable issues
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = "mongodb+srv://jayesh:jayesh%40123@cluster0.9usafbn.mongodb.net/pc_db?retryWrites=true&w=majority&appName=Cluster0";
+}
+
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
